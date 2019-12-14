933c3c303ad8062b9ec7bdcc9584b7c4c888ea47
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-12-14 15:08:31 +0100

    Add tests for Java, Bash and FPC.

---
+++ tests/bash/script.sh
@@ -0,0 +1,2 @@
+read name
+echo hello, $name
---
+++ tests/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        System.out.printf("hello, %s\n", name);
+    }
+}
